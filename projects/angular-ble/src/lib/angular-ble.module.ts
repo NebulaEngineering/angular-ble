5637b5b22e6ab5160d1af53bb569a8823b6da114
Daniel Tobon <daniel.tobon@nebulae.com.co>
2018-08-22 10:34:38 -0500

    added started bluetooth lib

--- projects/angular-ble/src/lib/angular-ble.module.ts
+++ projects/angular-ble/src/lib/angular-ble.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { AngularBleComponent } from './angular-ble.component';
+import { WebBluetoothModule } from '@manekinekko/angular-web-bluetooth';
 
 @NgModule({
   imports: [
+    WebBluetoothModule.forRoot({
+      enableTracing: false
+    })
   ],
   declarations: [AngularBleComponent],
   exports: [AngularBleComponent]
